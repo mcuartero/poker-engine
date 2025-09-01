bfe5e2ae8961be9fe7eeb0f865ff9b66952eb88f
mcuartero <m.cua3006@gmail.com>
2025-09-01 19:20:15 +1000

    add game.py and main.py and create blank GUI

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import pygame
 
 WIDTH, HEIGHT = 1280, 720
 FPS = 60
+TITLE = "Poker Engine"
 
 CARD_SIZE = (64, 89)
---
+++ game.py
@@ -0,0 +1,20 @@
+import pygame
+from config import WIDTH, HEIGHT, TITLE, FPS
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(TITLE)
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        
+        self.running = True
+    
+    def run(self):
+        while self.running:
+            dt = self.clock.tick(FPS) / 1000.0
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+        pygame.quit()
