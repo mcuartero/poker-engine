409abc6a300c5b9f156e1e0c3c00fc59cef15782
mcuartero <m.cua3006@gmail.com>
2025-09-01 19:33:47 +1000

    add configs

--- config.py
+++ config.py
@@ -4,4 +4,10 @@ WIDTH, HEIGHT = 1280, 720
 FPS = 60
 TITLE = "Poker Engine"
 
+BG_COLOUR = (20, 20, 20)
+TABLE_COLOUR = (80, 200, 120)
+TEXT_COLOUR = (235, 235, 235)
+
+FONT_NAME = pygame.font.get_default_font()
+
 CARD_SIZE = (64, 89)
